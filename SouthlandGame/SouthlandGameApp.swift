63af2f8834bae1a7f130a64326dd1857da5f9b93
Developer <Lepiozdyx@gmail.com>
2025-06-15 11:32:19 -0300

    Initial Commit

---
+++ SouthlandGame/SouthlandGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SouthlandGameApp.swift
+//  SouthlandGame
+//
+//  Created by Alex on 15.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SouthlandGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
